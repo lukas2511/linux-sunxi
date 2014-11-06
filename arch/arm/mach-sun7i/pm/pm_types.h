cac1d69de6871f2924f807723330be18a939086c
Lukas Schauer <lukas@schauer.so>
2014-11-06 20:19:26 +0000

    archlinux patches

--- arch/arm/mach-sun7i/pm/pm_types.h
+++ arch/arm/mach-sun7i/pm/pm_types.h
@@ -8,12 +8,20 @@
  * under the terms of the GNU General Public License version 2 as published by
  * the Free Software Foundation.
  */
-
-#include <linux/types.h>
-
+ 
 #ifndef __uxx_sxx_name
 #define __uxx_sxx_name
+typedef signed char         __s8;
+typedef unsigned char       __u8;
+typedef signed short        __s16;
+typedef unsigned short      __u16;
+typedef signed int          __s32;
+typedef unsigned int        __u32;
+typedef signed long long    __s64;
+typedef unsigned long long  __u64;
 
+typedef unsigned int		size_t;
+//typedef unsigned int		ptrdiff_t;
 //------------------------------------------------------------------------------
 //return value defines
 //------------------------------------------------------------------------------
@@ -22,6 +30,7 @@
 #define TRUE	(1)
 #define	FALSE	(0)
 
-#endif
+
+#endif 
 
 #endif /*_PM_TYPES_H*/
